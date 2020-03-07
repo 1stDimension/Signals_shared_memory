9133a0922306b38be4fe83cea36d7c4f7154dd4f
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-03-08 00:45:35 +0100

    Addign loop displaying all loaded values

--- main.c
+++ main.c
@@ -26,6 +26,11 @@ int main(int argc, char** argv){
     ;
 
   fclose(inputFile);
+
+  for (int i = 0; i < vectorSize; i++)
+    printf("%d", vector[i]);
+  printf("\n");
+
   for (int i = 0; i < childrenCount; i++){
 
     int f = fork();
